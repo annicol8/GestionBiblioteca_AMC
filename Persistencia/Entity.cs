4abf1ada75b13e24e04e716605e63b5697bb9a40
clara <clhernca@unirioja.es>
2025-11-19 20:26:12 +0100

    Creada persistencia

---
+++ Persistencia/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    internal class Entity<T>
+    {
+        private T clave;
+
+        public  Entity (T clave){
+            this.clave = clave;
+        }
+        public T Clave
+        {
+            get
+            {
+                return this.clave;
+            }
+        }
+    }
+    
+}
