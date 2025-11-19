a1bd34ae59dd2ed83fc7f50f1ef1be3a44c4cfef
clara <clhernca@unirioja.es>
2025-11-19 19:28:41 +0100

    Cambios en documento

---
+++ ModeloDominio/Documento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    internal abstract class Documento
+    {
+        // Atributos
+        private string isbn;           // Clave primaria
+        private string titulo;
+        private string autor;
+        private string editorial;
+        private int anoEdicion;
+
+        // Propiedades públicas
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Editorial { get; set; }
+        public int AnoEdicion { get; set; }
+
+        // Relación
+        public List<Ejemplar> Ejemplares { get; set; }
+
+        // Método abstracto
+        public abstract int DiasPrestamoPermitidos();
+
+    }
+}
---
+++ ModeloDominio/Ejemplar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    internal class Ejemplar
+    {
+        private string codigoEjemplar;
+        private string estado;
+        private Documento documentoAsociado;
+    }
+}
