96b7dcbc91a8018dc4093f59d18eba355039bfe0
Manuel Calvo <macalvl@unirioja.es>
2025-11-19 19:26:39 +0100

    Usuario añadido

---
+++ ModeloDominio/Usuario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDominio
+{
+    internal class Usuario
+    {
+        private string dni;
+        private string nombre;
+        private bool dadoAlta;
+
+        public Usuario(string dni, string nombre, bool dadoAlta)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.dadoAlta = dadoAlta;
+        }
+
+        public string getDni()
+        {
+            return this.dni;
+        }
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public bool estaDadoAlta()
+        {
+            return this.dadoAlta;
+        }
+
+        public void actualizarAlta(bool b)
+        {
+            this.dadoAlta = b;
+        }
+
+    }
+}
