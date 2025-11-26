55f3df87988370b812893b0874d55f02d393a563
clara <clhernca@unirioja.es>
2025-11-26 21:07:19 +0100

    claveprestamoejemplar

--- Persistencia/ClavePrestamoEjemplar.cs
+++ Persistencia/ClavePrestamoEjemplar.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    internal class ClavePrestamoEjemplar
+    internal class ClavePrestamoEjemplar : IEquatable<ClavePrestamoEjemplar>
     {
+        private int idPrestamo;
+        private int codigoEjemplar;
+
+        public int IdPrestamo { get; private set; }
+        public int CodigoEjemplar { get; private set; }
+
+        public ClavePrestamoEjemplar(int idPrestamo, int codigoEjemplar)
+        {
+            this.idPrestamo = idPrestamo;
+            this.codigoEjemplar = codigoEjemplar;
+
+        }
+
+        public bool Equals(ClavePrestamoEjemplar other)
+        {
+            return this.idPrestamo == other.idPrestamo &&
+                   this.codigoEjemplar == other.codigoEjemplar;
+        }
+
+        public override int GetHashCode()
+        {
+            return idPrestamo.GetHashCode() + codigoEjemplar.GetHashCode();
+        }
+
+
     }
 }
--- Persistencia/PrestamoEjemplarDato.cs
+++ Persistencia/PrestamoEjemplarDato.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    internal class PrestamoEjemplarDato
+    internal class PrestamoEjemplarDato : Entity<ClavePrestamoEjemplar>
     {
+        private int idPrestamo;         // los dos atributos o uno que sea clave?? y en el resto de clases, tienen que tener el atributo que se le pasa en Entity<...>
+        private int codigoEjemplar;
+        private DateTime fechaDevolucion;
+
+        public int IdPrestamo { get; private set; }
+        public int CodigoEjemplar { get; private set; }
+        public int ClavePrestamoEjemplar { get; private set; }
+        public DateTime FechaDevolucion { get; private set; }
+
+        public PrestamoEjemplarDato(int idPrestamo, int codigoEjemplar, DateTime fechaDevolucion) : base(new ClavePrestamoEjemplar(idPrestamo, codigoEjemplar))
+        {
+            this.idPrestamo = idPrestamo;
+            this.codigoEjemplar = codigoEjemplar;
+            this.fechaDevolucion = fechaDevolucion;
+        }
     }
 }
