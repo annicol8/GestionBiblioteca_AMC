b5bd542582e5a107a16ce74f6766b0c437661514
Manuel Calvo <macalvl@unirioja.es>
2025-11-26 21:04:32 +0100

    clase BD programada

--- Persistencia/AudioLibroDato.cs
+++ Persistencia/AudioLibroDato.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
-    internal class AudioLibroDato
+    internal class AudioLibroDato: Entity<String>
     {
         private string isbn; // Clave primaria
+        private string formatoDigital;
+        private int duracion;
+
+        
         public string Isbn { get; private set; }
-        public AudioLibroDato(string isbn) : base()
+        public AudioLibroDato(string isbn) : base(isbn)
         {
             Isbn = isbn;
         }
