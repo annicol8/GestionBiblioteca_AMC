c05057017a663f1b47f167f93aa1e62818aa3f6f
clara <clhernca@unirioja.es>
2025-12-10 20:58:38 +0100

    ln de personal sala empezada

--- LogicaNegocio/ILNPSala.cs
+++ LogicaNegocio/ILNPSala.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModeloDominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace LogicaNegocio
 {
     internal interface ILNPSala
     {
+        int AltaPrestamo(Prestamo prestamo);
+        //bool BajaPrestamo(Prestamo p);
+        Usuario GetUsuarioPrestamo(int idPrestamo);
+        EstadoPrestamo GetEstadoPrestamo(int idPrestamo);
+        List<Ejemplar> GetEjemplaresNoDevueltos(int idPrestamo);
+        Prestamo GetPrestamo(int idPrestamo);
+
+        List<Prestamo> GetPrestamosPorDocumento(string isbn);
+        //List<Prestamo> GetPrestamosPorUsuario(string dni);
+        void DevolverEjemplar(int idPrestamo, string codigoEjemplar);
+        List<Prestamo> GetPrestamosFueraDePlazo();
+        //bool HayEjemplaresDisponibles(string isbn);
+        //DateTime? GetFechaDisponibilidadDocumento(string isbn);
     }
 }
--- LogicaNegocio/LNPSala.cs
+++ LogicaNegocio/LNPSala.cs
@@ -10,8 +10,45 @@ namespace LogicaNegocio
     public class LNPSala : LNPersonal, ILNPSala
     {
        public LNPSala(PersonalSala personal) : base(personal) { }
-        
 
+        public int AltaPrestamo(Prestamo prestamo)
+        {
+            return Persistencia.Persistencia.AltaPrestamo(prestamo);
+        }
 
+        public void DevolverEjemplar(int idPrestamo, string codigoEjemplar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ejemplar> GetEjemplaresNoDevueltos(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EstadoPrestamo GetEstadoPrestamo(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prestamo GetPrestamo(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Prestamo> GetPrestamosFueraDePlazo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Prestamo> GetPrestamosPorDocumento(string isbn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetUsuarioPrestamo(int idPrestamo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
