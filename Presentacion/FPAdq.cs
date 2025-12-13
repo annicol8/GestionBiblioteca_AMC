55ed5d4f6bbe378ff98f7ca7579a21b56c18b6ad
annicol8 <ana.nicolas.ef@gmail.com>
2025-12-13 23:56:21 +0100

    unir

---
+++ Presentacion/FPAdq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModeloDominio;
+
+namespace Presentacion
+{
+    public partial class FPAdq : Form
+    {
+        //private ILNPAdq lnAdq;
+
+        public FPAdq(Personal p)//: base(p)
+        {
+            //this.lnAdq = new ILNPAdq(p);
+            InitializeComponent();
+        }
+    }
+}
