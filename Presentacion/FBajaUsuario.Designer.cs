8aae1aea20d951cdc2317ee7e4ec61dcd68cc952
clara <clhernca@unirioja.es>
2025-12-15 19:41:54 +0100

    formulario para baja de usuario creado

---
+++ Presentacion/FBajaUsuario.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Presentacion
+{
+    partial class FBajaUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nombre = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbDni = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(233, 259);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(87, 25);
+            this.Nombre.TabIndex = 7;
+            this.Nombre.Text = "Nombre";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(407, 254);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(165, 31);
+            this.tbNombre.TabIndex = 6;
+            // 
+            // tbDni
+            // 
+            this.tbDni.Location = new System.Drawing.Point(407, 165);
+            this.tbDni.Name = "tbDni";
+            this.tbDni.Size = new System.Drawing.Size(165, 31);
+            this.tbDni.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DNI: ";
+            // 
+            // FBajaUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbDni);
+            this.Controls.Add(this.label1);
+            this.Name = "FBajaUsuario";
+            this.Text = "FBajaUsuario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbDni;
+        private System.Windows.Forms.Label label1;
+    }
+}
