c1f75b65d381a159bd35c6db8d14a513ebebc8af
clara <clhernca@unirioja.es>
2026-01-02 13:50:44 +0100

    pre y post de devolver prestamo, eliminar form1

--- Presentacion/FDevolverPrestamo.cs
+++ Presentacion/FDevolverPrestamo.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using LogicaNegocio;
 using ModeloDominio;
@@ -20,13 +13,15 @@ namespace Presentacion
         {
             InitializeComponent();
         }
-
+        /* PRE: lnSala != null, prestamo != null
+   POST: Inicializa el formulario con los datos necesarios para devolver ejemplares de un préstamo */
         public FDevolverPrestamo(ILNPSala lnSala, Prestamo prestamo) : this()
         {
             this.lnSala = lnSala;
             this.prestamo = prestamo;
         }
-
+        /* PRE: lnSala y prestamo inicializados
+   POST: Carga en el listBox los ejemplares no devueltos del préstamo */
         private void FDevolverPrestamo_Load(object sender, EventArgs e)
         {
             listBoxEjemplares.Items.Clear();
@@ -39,9 +34,11 @@ namespace Presentacion
 
         private void CargarEjemplaresPendientes()
         {
-            
-        }
 
+        }
+        /* PRE: lnSala y prestamo inicializados, listBoxEjemplares con elementos cargados
+   POST: Si hay ejemplares seleccionados y el usuario confirma, los marca como devueltos
+         y cierra el formulario. Si no hay selección, muestra error */
         private void botonDevolver_Click(object sender, EventArgs e)
         {
             if (listBoxEjemplares.SelectedItems.Count == 0)
